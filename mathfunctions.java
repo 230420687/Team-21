19b60862f5203209e9c64750fe82b834eebc95f8
John Reardon <230427826@aston.ac.uk>
2024-10-21 17:13:36 +0100

    Added a min function

---
+++ mathfunctions.java
@@ -0,0 +1,18 @@
+/********************************
+Developer: John Reardon
+University ID: 230427826
+Function: This function takes two inputs as integers and returns the lower integer
+********************************/
+
+
+public int min(int a, int b){
+    if (a > b){
+        return a
+    }
+    elif (a==b){
+        return "both integers are equal"
+    }
+    else{
+        return b
+    }
+}

3ad6fbfbf546e43243ada378d6d0c89fa09aa30f
Dilax Balatharan <dilax511@hotmail.com>
2024-11-15 08:51:41 +0000

    Created the searchbar, backend functionality and created the search results page

--- Team-21/routes/web.php
+++ Team-21/routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SearchController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/searchbar', function(){
+    return view('search.form');
+});
+
+Route::get('/search', [SearchController::class, 'search'])->name('search');
+

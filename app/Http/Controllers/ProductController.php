ba38f7a7ab46f0f111cdd8d5cdb7c9acbef30745
John Reardon <230427826@aston.ac.uk>
2024-12-05 17:12:57 +0000

    Added functionality for category buttons, now link to unique blade views which only display products of that category. Also changed .env and database files back to local database rather than remote as remote is still not working.

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,13 +8,21 @@ class ProductController extends Controller
 {
     public function index()
     {
-        // Fetch all products from the database
+        // Retrieve all products from the database
         $products = Product::all();
 
         // Return a view and pass the data to it
         return view('products.index', compact('products'));
     }
-    public function show($product_id)
-{
+    public function show($product_id){
 return view('/productdesc', ['product' => Product::find($product_id)]);}
+
+    public function showlap(){
+        #$products = Product::where('category_id',1)->get()->toArray();
+
+        $products = Product::all();
+        return view('/Laptops', compact('products'));
+        #return view('/Laptops',compact('products'));
+    }
 }
+?>
